2ad7e81f8e8f8ef66ac7e1d15b11bfd7a3d7d90d
Shane <Shane@SHANE-PC>
2017-01-21 17:53:28 -0600

    advanced hand move

--- Waving Simulator/Assets/HandMovement.cs
+++ Waving Simulator/Assets/HandMovement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class HandMovement : MonoBehaviour {
@@ -8,6 +10,7 @@ public class HandMovement : MonoBehaviour {
 public bool move = false;
 public AnimationCurve curve ;
 public float var = 0;
+public float stamina = 100;
 	// Use this for initialization
 	void Start () {
 		curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 30));
@@ -19,24 +22,21 @@ public float var = 0;
 	void Update () {
 		if (Input.GetMouseButton(0))
 		{
-			transform.eulerAngles = new Vector3(90, 190, curve.Evaluate(var));
-			var += (float)0.5*Time.deltaTime;
+			if(transform.eulerAngles.x > 0 && transform.eulerAngles.x < 100)
+			{
+				transform.Rotate(-30*Time.deltaTime, 0, 0);
+			}
+			else
+			{
+				transform.eulerAngles = new Vector3(0, curve.Evaluate(var), 0);
+				var += (float)0.5 * Time.deltaTime;
+			}
+
 		}
-		/*if(!move)
+		else if(!Input.GetMouseButton(0) && transform.eulerAngles.x < 75)
 		{
-			transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
-			transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime);//(float)-0.5);
-			var++;
-			if(var%20 == 0)
-				move = true;
+			transform.Rotate(30*Time.deltaTime, 0, 0);
+			//Debug.Log(transform.eulerAngles.x);
 		}
-		else
-		{
-			transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
-			transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime);//(float)0.5);
-			var++;
-			if(var%20 == 0)
-				move = false;
-		}*/
 	}
 }
