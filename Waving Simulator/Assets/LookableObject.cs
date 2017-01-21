403d2ad58fa451f28db1818372cc79405a7482fe
JesusDanielRadcliff <anthonybeug@live.ca>
2017-01-21 16:15:06 -0600

    Added Lookable object

---
+++ Waving Simulator/Assets/LookableObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookableObject : MonoBehaviour {
+
+    //Public variables
+    public float pointsEarned = 10.0f;
+    public float startingHealth = 100.0f;
+    public float healthDecreaseAmount = 5.0f;//Multiplied by dtime
+    public PointsManager pointsManager;
+    
+    private float health;
+    private bool isLookedAt = false;
+    private bool isAlive = true;
+
+    //Call when looking at this object. It will do its thing in Update
+    public void LookAt()
+    {
+        isLookedAt = true;
+    }
+	
+    public float GetHealth()
+    {
+        return health;
+    }
+
+    private void Start()
+    {
+        health = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (isLookedAt && isAlive)
+        {
+            health -= healthDecreaseAmount * Time.deltaTime;
+
+            if (health <= 0.0f)
+            {
+                pointsManager.AddPoints(pointsEarned);
+                isAlive = false;
+            }
+        }
+        isLookedAt = false;
+	}
+}
--- Waving Simulator/Assets/PointsManager.cs
+++ Waving Simulator/Assets/PointsManager.cs
@@ -5,12 +5,31 @@ using UnityEngine.UI;
 
 public class PointsManager : MonoBehaviour
 {
-    private float points = 0;
     public Text pointsText;
+    public float fontBadSize = 12;
+    public float fontSizeNormal = 14;
+    public float fontSizeGood = 16;
+    public float colorFadePerFrame = 0.05;
+
+    private float points = 0;
+    private Color color = Color.white;
+    private float fontSize;
 
     public void AddPoints(float amount)
     {
         points += amount;
+        points = Mathf.min(0, points);
+
+        if (amount < 0)
+        {
+            color = Color.red;
+            fontSize = fontSizeBad;
+        }
+        else if (ammount > 0)
+        {
+            color = Color.green;
+            fontSize = fontSizeGood;
+        }
     }
 
     public void ResetPoints()
@@ -21,15 +40,24 @@ public class PointsManager : MonoBehaviour
     void Start()
     {
         SetText();
+        fontSize = fontSizeNormal;
     }
 
     void FixedUpdate()
     {
         SetText();
+        
+        fontSize =
+
+        color.r = Mathf.Max(color.r + colorFadePerFrame, 1.0f);
+        color.g = Mathf.Max(color.g + colorFadePerFrame, 1.0f);
+        color.b = Mathf.Max(color.b + colorFadePerFrame, 1.0f);
     }
 
     private void SetText()
     {
         pointsText.text = "Points: " + points.ToString();
+        pointsText.fontSize = fontSize;
+        pointsText.color = color;
     }
 }
