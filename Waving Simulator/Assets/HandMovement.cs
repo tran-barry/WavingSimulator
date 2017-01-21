4f78a75542eb6b8c33d124b482365cccd01796df
Shane <Shane@SHANE-PC>
2017-01-21 14:00:19 -0600

    hand better move

--- Waving Simulator/Assets/HandMovement.cs
+++ Waving Simulator/Assets/HandMovement.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class HandMovement : MonoBehaviour {
 
 public bool move = false;
-public int var = 0;
+public AnimationCurve curve ;
+public float var = 0;
 	// Use this for initialization
 	void Start () {
-		
+		curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 30));
+		curve.preWrapMode = WrapMode.PingPong;
+		curve.postWrapMode = WrapMode.PingPong;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!move)
+		if (Input.GetMouseButton(0))
+		{
+			transform.eulerAngles = new Vector3(90, 190, curve.Evaluate(var));
+			var += (float)0.5*Time.deltaTime;
+		}
+		/*if(!move)
 		{
 			transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
 			transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime);//(float)-0.5);
@@ -29,6 +37,6 @@ public int var = 0;
 			var++;
 			if(var%20 == 0)
 				move = false;
-		}
+		}*/
 	}
 }
