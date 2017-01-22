9a027d2d63e9e8f5fed6cfe99109e45f0a206fc9
JesusDanielRadcliff <anthonybeug@live.ca>
2017-01-22 11:47:45 -0600

    Added start screen UI

--- Waving Simulator/Assets/PlayerController.cs
+++ Waving Simulator/Assets/PlayerController.cs
@@ -6,9 +6,11 @@ using System.IO;
 using System.Text;
 
 public class PlayerController : MonoBehaviour {
-	float speed = 0;
+	float speed = 1;
 	float startPos = 0;
 	float endPos = 10;
+    private bool gameStarted = false;
+    public GameObject startScreenUI;
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +19,24 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Move forward a little
-		transform.Translate(new Vector3(0,0,1) * speed *  Time.deltaTime);
 
-		// If the position is past a specific point, then warp back
-		Vector3 position = transform.position;
-		var warp = startPos - position.z;
-		if (position.z > endPos) {
-			transform.Translate(new Vector3(0,0,warp));
-		}
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameStarted = true;
+            startScreenUI.SetActive(false);
+        }
+
+        if (gameStarted)
+        {
+            // Move forward a little
+		    transform.Translate(new Vector3(0,0,1) * speed *  Time.deltaTime);
+
+		    // If the position is past a specific point, then warp back
+		    Vector3 position = transform.position;
+		    var warp = startPos - position.z;
+		    if (position.z > endPos) {
+			    transform.Translate(new Vector3(0,0,warp));
+		    }
+        }
 	}
 }
