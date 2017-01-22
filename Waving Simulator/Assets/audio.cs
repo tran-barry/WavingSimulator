85d5497ada318ee84827595ea3dc720067547254
Downmagic <Downmagic@DOWNY-LAPTOP>
2017-01-22 13:53:14 -0600

    auido updaTE

---
+++ Waving Simulator/Assets/audio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audio : MonoBehaviour {
+
+	public AudioSource godSaveTheQueen;
+	public AudioSource gunSounds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check the z coordinate
+		if(transform.position.z >= 2.8 || transform.position.z < -8){
+			//play god save the queen
+			//gunSounds.Stop();
+			godSaveTheQueen.Play();
+			
+		}else{
+			//play gun shots 
+			//godSaveTheQueen.Stop();
+			gunSounds.Play();
+		}
+	}
+}
