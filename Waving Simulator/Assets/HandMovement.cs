bb45fdaee6aba8c09f2f973a922035b3533951cc
Shane <Shane@SHANE-PC>
2017-01-21 19:57:34 -0600

    ading stamina to hand move

--- Waving Simulator/Assets/HandMovement.cs
+++ Waving Simulator/Assets/HandMovement.cs
@@ -4,23 +4,33 @@ using System;
 using System.IO;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandMovement : MonoBehaviour {
-
 public bool move = false;
 public AnimationCurve curve ;
 public float var = 0;
+
 public float stamina = 100;
+private float fontSize;
+private Color color;
+public Color colorNeutral = Color.green;
+public float fontSizeNormal = 16;
+public Text staminaText;
+	// Use this for initialization
 	// Use this for initialization
 	void Start () {
 		curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 30));
 		curve.preWrapMode = WrapMode.PingPong;
 		curve.postWrapMode = WrapMode.PingPong;
+		SetText();
+        fontSize = 16;
+        color = colorNeutral;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton(0) && stamina > 0)
 		{
 			if(transform.eulerAngles.x > 0 && transform.eulerAngles.x < 100)
 			{
@@ -38,5 +48,25 @@ public float stamina = 100;
 			transform.Rotate(30*Time.deltaTime, 0, 0);
 			//Debug.Log(transform.eulerAngles.x);
 		}
+		if(stamina > 0 && Input.GetMouseButton(0))
+		{
+			stamina -= 30 * Time.deltaTime;
+			if(stamina < 0)
+				stamina = 0;
+		}
+		if(stamina < 100 && !Input.GetMouseButton(0))
+		{
+			stamina += 30 * Time.deltaTime;
+			if(stamina > 100)
+				stamina = 100;
+		}
+		SetText();
 	}
+	private void SetText()
+    {
+		int s = (int)stamina;
+		staminaText.text = "Stamina: " + s.ToString();
+        staminaText.fontSize = (int)fontSize;
+        staminaText.color = color;
+    }
 }
