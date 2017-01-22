16025338419d9caad2680d2ba34f898cc9a26373
Bellio <Downmagic@hotmail.com>
2017-01-21 19:45:26 -0600

    Small changes

---
+++ Waving Simulator/Assets/LookAt/Materials/RotateClouds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateClouds : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	void Start () {
+
+		transform.Rotate(Vector3.right * 300 *  Time.deltaTime) ;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		// Move forward a little
+	}
+}
